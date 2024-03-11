2e69f580cd84ebfb86a807af7cbc69faa0c2e3eb
DSY_xiaoluo <89125608+DSY-xiaoluo@users.noreply.github.com>
2024-03-12 01:21:50 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -1,13 +1,21 @@
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", function () {
   var xhr = new XMLHttpRequest();
   xhr.open('GET', '/content/sidebar.html', true);
-  xhr.onreadystatechange = function() {
-      if (xhr.readyState === 4 && xhr.status === 200) {
-          var sidebarElements = document.querySelectorAll('#sidebar');
-          for (var i = 0; i < sidebarElements.length; i++) {
-              sidebarElements[i].innerHTML = xhr.responseText;
-          }
+  xhr.onreadystatechange = function () {
+    if (xhr.readyState === 4 && xhr.status === 200) {
+      var sidebarElements = document.querySelectorAll('#sidebar');
+      for (var i = 0; i < sidebarElements.length; i++) {
+        sidebarElements[i].innerHTML = xhr.responseText;
       }
+    }
   };
   xhr.send();
 });
+
+function performSearch() {
+  var searchTerm = document.getElementById('searchInput').value;
+  var selectedEngine = document.getElementById('searchEngine').value;
+  var searchUrl = selectedEngine + encodeURIComponent(searchTerm);
+  window.open(searchUrl, '_blank');
+  return false;
+}
